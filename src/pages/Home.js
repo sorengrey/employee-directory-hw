bcdea569878827757c9e43275839d9f8568a68c1
Heather Smith <halexsmith86@gmail.com>
2021-04-28 17:01:17 -0400

    Styling for navbar and button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Navbar from "../components/Navbar";
 import Container from "../components/Container";
 import Wrapper from "../components/Wrapper";
 import Row from "../components/Row";
@@ -11,7 +10,7 @@ function Home() {
         <Container style={{ marginTop: 30 }}>
           <h1 className="text-center">Welcome to Employee Directory!</h1>
           <h3 className="text-center">Click the button below to get a table of your employees!</h3>
-          <button type="button" className="btn btn-success">Generate Table</button>
+          <center><button type="button" className="btn btn-success">Generate Table</button></center>
         </Container>
       </div>
           )}
