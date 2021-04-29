9a3a94fbad6a27f6df36e5a414bac44d460019d6
Heather Smith <halexsmith86@gmail.com>
2021-04-29 13:33:14 -0400

    Added Table component with dummy data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
 import Container from "../components/Container";
-import Wrapper from "../components/Wrapper";
-import Row from "../components/Row";
-import Col from "../components/Col";
+import Table from "../components/Table";
 
 function Home() {
-    return (
-      <div>
-        <Container style={{ marginTop: 30 }}>
-          <h1 className="text-center">Welcome to Employee Directory!</h1>
-          <h3 className="text-center">Click the button below to get a table of your employees!</h3>
-          <center><button type="button" className="btn btn-success">Generate Table</button></center>
-        </Container>
-      </div>
-          )}
+  return (
+    <div>
+      <Container style={{ marginTop: 30 }}>
+        <h1 className="text-center">Welcome to your Employee Directory!</h1>
+
+        <h3 className="text-center">
+          Click the button below to get a table of your employees!
+        </h3>
+
+        <br />
+
+        <center>
+          <button type="button" className="btn btn-success">
+            Generate Table
+          </button>
+
+          <br />
+          <br />
+
+          <Table />
+        </center>
+      </Container>
+    </div>
+  );
+}
 
 export default Home;
