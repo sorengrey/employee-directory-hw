5035c17591426531aaa9eb521175eded8f48982b
Heather Smith <halexsmith86@gmail.com>
2021-04-30 13:59:24 -0400

    Changes to App.js, API object is now usable

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,19 @@ import Navbar from "./components/Navbar";
 import Wrapper from "./components/Wrapper";
 import Home from "./pages/Home";
 
+require("es6-promise").polyfill();
+require("isomorphic-fetch");
+
 export default function App() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
   const [q, setQ] = useState("");
-  
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=10&inc=picture,id,name,email,cell,location")
+    .then(response => response.json())
+    .then(json => setData(json));
+  }, [])
+
   return (
     <Router>
       <div>
