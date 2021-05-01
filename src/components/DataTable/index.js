9f280d92e0d237be7bc65877dd60f09237e77977
Heather Smith <halexsmith86@gmail.com>
2021-05-01 16:48:49 -0400

    Styled navbar, set up table columns

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -1,13 +1,28 @@
-
-import React, { Component } from "react";
+import React from "react";
 
 function DataTable(props){
   return(
-    <table className="table">
-       <thead></thead>
-      <tbody></tbody>
-    </table>
-  )
+    <div>
+    <br />
+    <br />
+    <table className="table table-dark table-sortable text-center">
+    <thead>
+      <tr>
+        <th scope="col">Image</th>
+        <th scope="col" data-field="name" data-sortable="true">Name</th>
+        <th scope="col" data-field="email" data-sortable="true">Email</th>
+        <th scope="col" data-field="email" data-sortable="true">Cell</th>
+        <th scope="col" data-field="email" data-sortable="true">Location</th>
+        
+        
+        </tr>
+        </thead>
+        <tbody>
+          <td></td>
+        </tbody>
+        </table>
+        </div>
+        )
 }
 
 
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import "./style.css";
+import './style.css';
 
-// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-     
-        Employee Directory
-
+    <nav>
+        <h1>Employee Directory</h1>
     </nav>
   );
 }
