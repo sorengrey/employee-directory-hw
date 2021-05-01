1f496980a477972e1495b622f6f91e14452cc313
Heather Smith <halexsmith86@gmail.com>
2021-05-01 17:08:27 -0400

    Formatted data table

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -1,27 +1,32 @@
 import React from "react";
 
-function DataTable(props){
-  return(
+function DataTable(props) {
+  return (
     <div>
-    <table className="table table-dark table-sortable text-center">
-    <thead>
-      <tr>
-        <th scope="col">Image</th>
-        <th scope="col" data-field="name" data-sortable="true">Name</th>
-        <th scope="col" data-field="email" data-sortable="true">Email</th>
-        <th scope="col" data-field="email" data-sortable="true">Cell</th>
-        <th scope="col" data-field="email" data-sortable="true">Location</th>
-        
-        
-        </tr>
+      <table className="table table-dark table-sortable text-center">
+        <thead>
+          <tr>
+            <th scope="col">Image</th>
+            <th scope="col" data-field="name" data-sortable="true">
+              Name
+            </th>
+            <th scope="col" data-field="email" data-sortable="true">
+              Email
+            </th>
+            <th scope="col" data-field="email" data-sortable="true">
+              Cell
+            </th>
+            <th scope="col" data-field="email" data-sortable="true">
+              Location
+            </th>
+          </tr>
         </thead>
         <tbody>
           <td></td>
         </tbody>
-        </table>
-        </div>
-        )
+      </table>
+    </div>
+  );
 }
 
-
- export default DataTable;
+export default DataTable;
