6c01b10b6cb8c723f8f33f984a8c6f212976f01b
Heather Smith <halexsmith86@gmail.com>
2021-05-01 17:00:57 -0400

    Changes made for gh-pages deployment

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -3,8 +3,6 @@ import React from "react";
 function DataTable(props){
   return(
     <div>
-    <br />
-    <br />
     <table className="table table-dark table-sortable text-center">
     <thead>
       <tr>
